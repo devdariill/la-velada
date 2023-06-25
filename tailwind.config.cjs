50ec8c27f308fac45abfbecdc1d30dbf308592e2
nATOr <gowluonade@gmail.com>
2023-06-25 07:30:02 -0500

    font-load

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				// boxing:['Boxing','sans-serif'],
+				// 'boxing-striped':['Boxing Striped','sans-serif'],
+				'facon':['Facon','sans-serif'],
+			}
+		},
 	},
 	plugins: [
 		require('tailwindcss-animated')
